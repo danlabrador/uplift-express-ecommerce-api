06819688033c24abb73839dedcf0328f24b067c2
Dan Labrador <dan.labrador@myamazonguy.com>
2024-05-25 15:13:17 -0400

    feat: add PUT /api/v1/products/:id endpoint

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -1,5 +1,6 @@
 import getProducts from "./products/getProducts.js";
 import getProduct from "./products/getProduct.js";
 import createProduct from "./products/createProduct.js";
+import updateProduct from "./products/updateProduct.js";
 
-export { getProducts, getProduct, createProduct };
+export { getProducts, getProduct, createProduct, updateProduct };
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { createProduct, getProduct, getProducts } from '../controllers/products.controllers.js';
+import { createProduct, getProduct, getProducts, updateProduct } from '../controllers/products.controllers.js';
 
 const productsRouter = Router();
 
 productsRouter.get('/', getProducts);
 productsRouter.get('/:id', getProduct);
 productsRouter.post('/', createProduct);
+productsRouter.put('/:id', updateProduct);
 
 export default productsRouter;
